76cc9401cfb09e79f9bdc410103ab60795cf0b93
Isak Buhl-Mortensen <buhlmort@nbi.ku.dk>
2016-10-30 22:42:01 +0100

    slight changes

--- fragment_shader.fs
+++ fragment_shader.fs
@@ -15,6 +15,9 @@ uniform float resy; // height of window in pixels
 uniform float frameTime; // time running ~ frames
 uniform float frustumD;
 
+uniform float logCoeffDE;
+uniform float powerParam;
+
 // camera variables
 uniform vec3 posVec;
 uniform vec3 viewVec;
@@ -25,7 +28,8 @@ uniform vec3 rightVec;
 uniform int fractalMaxIt;
 uniform int marchMaxIt;
 uniform float marchEpsilon;
-
+uniform float lightAdjust;
+uniform float maxFractalDist;
 // Pixel coordinates
 float Xc = gl_FragCoord.x - 0.5*resx; // Centralized FOV x coordinate
 float Yc = gl_FragCoord.y - 0.5*resy; // Centralized FOV y coordinate
@@ -40,7 +44,7 @@ vec3 rayDir = normalize(frustumD*viewVec + Xc*rightVec + Yc*upVec);
 // 	return length(p) - r;
 // }
 
-#define POWER_PARAM  4.0
+
 #define MAX_FRACTAL_DIST 1.2
 
 #define MAX_MARCH 5.0
@@ -50,18 +54,58 @@ vec3 rayDir = normalize(frustumD*viewVec + Xc*rightVec + Yc*upVec);
 
 // vec3 getNormal(vec3 p, float d)
 // {
-// 	vec3 x = vec3(1,0,0);
-// 	vec3 y = vec3(0,1,0);
-// 	vec3 z = vec3(0,0,1);
+	vec3 ex = vec3(1,0,0);
+	vec3 ey = vec3(0,1,0);
+	vec3 ez = vec3(0,0,1);
 
 // 	return vec3(0,0,0);//normalize(vec3(masterDist(p + d*x), masterDist(p + d*y), masterDist(p + d*z)) - masterDist(p)*vec3(1,1,1));
 // }
 
+
+float DE(vec3 ray)
+{
+// return 2;
+	vec3 z = ray;
+	float r = 0.0;  // r =  |f(0;c) | = | z | ,    					where  f(n;c) = f(n-1;c)^2 + c    and of course  f(0;c) = 0
+	float dr = 1.0; // dr = |f'(0;c)| = 2 | prevz | prevdr + 1 ,    computes to  f'(n;c) = 2 f(n-1;c)f'(n-1;c) + 1
+	
+	int escapeTime = 0;
+
+
+	for (int ii = 0; ii < fractalMaxIt ; ii++) { 
+		r = length(z);
+		if (r > maxFractalDist)
+		{
+			escapeTime = ii;
+			break;
+		}
+		
+		// convert to polar coordinates
+		float theta = acos(z.z/r);
+		float phi = atan(z.y,z.x);
+		float rpow = pow(r,powerParam - 1.0);
+		dr = rpow*powerParam*dr + 1.0;	//dr =  pow( r, powerParam - 1.0)*powerParam*dr + 1.0;
+
+		
+		// scale and rotate the point
+		float zr =  rpow*r; //pow( r, powerParam);
+		theta = theta*powerParam;
+		phi = phi*powerParam;
+
+		
+		// convert back to cartesian coordinates
+		z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
+		z += ray;
+		
+	}
+	return logCoeffDE*log(r)*r/dr;
+}
+
 void main() {
 
-	vec3 fogColor = vec3(0.5,0.5,0.5);
-	fogColor = vec3(1.0,0.0,0.0);
-	fogColor = fragmentColor.xyz;
+	vec3 fogColor = vec3(0.0,0.0,0.0);
+
+	
 	color = fogColor;
 
 	float t = 0.0;
@@ -74,7 +118,8 @@ void main() {
 
 	vec3 ray, lightRay;
 
-	float maxFractalDist = 3.0;
+
+	float shade = 0.0;
 
 
 	// Creat loop over 2 x 2 collection of rays and average for anti-aliasing
@@ -82,43 +127,8 @@ void main() {
 		{
 			ray = posVec + t*rayDir;
 					
-			// return 2;
-			vec3 z = ray;
-			float r = 0.0;  // r =  |f(0;c) | = | z | ,    					where  f(n;c) = f(n-1;c)^2 + c    and of course  f(0;c) = 0
-			float dr = 1.0; // dr = |f'(0;c)| = 2 | prevz | prevdr + 1 ,    computes to  f'(n;c) = 2 f(n-1;c)f'(n-1;c) + 1
+			d = DE(ray);
 			
-			int escapeTime = 0;
-			float shade = 0.0;
-
-			for (int ii = 0; ii < fractalMaxIt ; ii++) { 
-				r = length(z);
-				if (r > maxFractalDist)
-				{
-					escapeTime = ii;
-					break;
-				}
-				
-				// convert to polar coordinates
-				float theta = acos(z.z/r);
-				float phi = atan(z.y,z.x);
-				float rpow = pow(r,POWER_PARAM - 1.0);
-				dr = rpow*POWER_PARAM*dr + 1.0;	//dr =  pow( r, POWER_PARAM - 1.0)*POWER_PARAM*dr + 1.0;
-
-				
-				// scale and rotate the point
-				float zr =  rpow*r; //pow( r, POWER_PARAM);
-				theta = theta*POWER_PARAM;
-				phi = phi*POWER_PARAM;
-
-				
-				// convert back to cartesian coordinates
-				z = zr*vec3(sin(theta)*cos(phi), sin(phi)*sin(theta), cos(theta));
-				z += ray;
-				
-			}
-
-			d = 0.5*log(r)*r/dr;
-
 			if (d < marchEpsilon)   // Criteria for being close enough to the fractal
 			{
 				// blend = exp(5.0*(t/MAX_MARCH - 1.0)); // Blending for fogging out
@@ -128,11 +138,14 @@ void main() {
 				// float shade = -dot(getNormal(ray,marchEp), lightVector);
 
 				// shade = 1.0 - 1.5*escapeTime/float(fractalMaxIt);
-				shade = 1.0 - i/float(marchMaxIt);
+
+				float param = min(i/float(marchMaxIt), 1.0);
+
+				shade = 1.0 - param;
 				if (shade < 0.0) shade = 0.0;
-				shade = exp(5.0*(shade - 1.0));
+				shade = exp(5.0*(shade-1.0));
 				
-				color = shade*vec3(0.8,0.9,0.9) + (1.0 - shade)*fogColor ;//+ blend*fogColor;// blockFactor*shade*(1.0 - blend)*vec3(1.0,0.0,0.0) + blend*fogColor;
+				color = lightAdjust*(shade*vec3(0.8,0.9,0.9) + (1.0 - shade)*fogColor) ;//+ blend*fogColor;// blockFactor*shade*(1.0 - blend)*vec3(1.0,0.0,0.0) + blend*fogColor;
 
 				break;
 			}
